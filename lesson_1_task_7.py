dcc4e3060d134b158b3865cddd8a6c78f0167aea
VeraGrechina <vera0290@yandex.ru>
2023-09-28 20:28:40 +0300

    Первое задание

---
+++ lesson_1_task_7.py
@@ -0,0 +1,43 @@
+def  one_1():
+    print(1)
+
+def two_2():
+    print(2)
+
+def three_3():
+    print(3)
+
+def four_4():
+    print(4)
+
+def five_5():
+    print(5)
+
+def six_6():
+    print(6)
+
+def seven_7():
+    print(7)
+
+def eight_8():
+    print(8)
+
+def nine_9():
+    print(9)
+
+def zero_0():
+    print(0)
+
+eight_8()
+eight_8()
+zero_0()
+zero_0()
+five_5()
+five_5()
+five_5()
+three_3()
+five_5()
+three_3()
+five_5()
+
+
